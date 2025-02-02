9bce3481e6ffeb98c80cd29695b55b7d4f7446e1
tie993993333 <tie993993333@icloud.com>
2025-02-02 21:05:52 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
-TOKEN = "7243886679:AAHnPUH_-OrLMjfotS2vzrLSny4nKB0yJdc"
-ADMIN_IDS = [7597393338]  # 管理员 Telegram 用户 ID 列表
+TOKEN = ""
+ADMIN_IDS = []  # 管理员 Telegram 用户 ID 列表
 
 # 充值地址池（建议使用 TRON 地址的 16 进制格式）
 DEPOSIT_ADDRESSES = [
@@ -12,4 +12,4 @@ DEPOSIT_ADDRESSES = [
 TRON_API_BASE = "https://api.trongrid.io"
 
 # API 金鑰（用于请求 TRON API 的安全认证）
-API_KEY = "0a798c7f-baad-4046-a669-0f23088f279d"
+API_KEY = ""
